3150cb16bea3ebf520d05a9d0d1295fef8fb6a11
chenhao <chenhao@zichan360.com>
2019-09-26 13:13:46 +0800

    Initial commit

---
+++ src/main/java/cn/forward3/weekup/WeekupApplication.java
@@ -0,0 +1,13 @@
+package cn.forward3.weekup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/forward3/weekup/WeekupApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.forward3.weekup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeekupApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
